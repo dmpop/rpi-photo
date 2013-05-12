4ab6df45f2d4d812e46a52c33032daeb61178f89
Dmitri Popov <dmpop@linux.com>
2013-05-12 13:15:18 +0200

    auto-download.sh added.

---
+++ auto-download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#Wait for camera
+
+DEVICE=$(gphoto2 --auto-detect | grep usb | cut -b 36-42 | sed 's/,/\//')
+while [ -z ${DEVICE} ]
+	do
+	sleep 1
+	DEVICE=$(gphoto2 --auto-detect | grep usb | cut -b 36-42 | sed 's/,/\//')
+done
+
+cd /media/usb0/
+mkdir "`date --iso-8601`" && cd $_
+gphoto2 --get-all-files --filename "%Y%m%d-%H%M%S-%03n.%C"
+cd ~
+halt
