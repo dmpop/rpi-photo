c9fd81923a4e69318423644cf19a3f4d150d91c8
Dmitri Popov <dmpop@linux.com>
2013-05-13 16:56:18 +0200

    LED and shutdown code added.

--- fetch.py
+++ fetch.py
@@ -5,9 +5,16 @@ import os
 import RPi.GPIO as GPIO
  
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(25, GPIO.OUT)
+GPIO.output(25, True)
  
 while True:
-	if ( GPIO.input(17) == False ):
+	if (GPIO.input(17) == False ):
 		os.system('./get-all-files.sh')
+		GPIO.output(25, False)
+	if (GPIO.input(23) == False):
+		os.system('sudo halt')
         sleep(0.1);
