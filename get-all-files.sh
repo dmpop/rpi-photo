cfb80e438f9e6a12ef28f5b422fe6ec5349ae125
Dmitri Popov <dmpop@linux.com>
2013-05-14 14:37:14 +0200

    Switched to ExifTool for file renaming.

--- get-all-files.sh
+++ get-all-files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #mkdir "`date +%F--%T`" && cd $_
 mkdir "`date --iso-8601`" && cd $_
-gphoto2 --get-all-files --filename "%Y%m%d-%H%M%S-%03n.%C"
+gphoto2 --get-all-files --filename
+exiftool -r -d %Y%m%d-%H%M%S.%%e "-FileName<DateTimeOriginal" .
 cd ~
