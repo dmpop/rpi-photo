4094bc46ec4b9b47660ee4f32637de8b12d2337e
Dmitri Popov <dmpop@linux.com>
2013-05-14 14:30:05 +0200

    Comments added.

--- auto-download.sh
+++ auto-download.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-#Wait for camera
-
+# Wait for camera
 DEVICE=$(gphoto2 --auto-detect | grep usb | cut -b 36-42 | sed 's/,/\//')
 while [ -z ${DEVICE} ]
 	do
@@ -9,10 +8,12 @@ while [ -z ${DEVICE} ]
 	DEVICE=$(gphoto2 --auto-detect | grep usb | cut -b 36-42 | sed 's/,/\//')
 done
 
+# Switch to /media/usb0 and create a directory with current date as its name
 cd /media/usb0/
 mkdir "`date --iso-8601`" && cd $_
-#gphoto2 --get-all-files --filename "%Y%m%d-%H%M%S-%03n.%C"
+# Transfer files to a USB storage device mounted at /media/usb0
 gphoto2 --get-all-files
+# Rename files using ExifTool based on EXIF date and time data
 exiftool -r -d %Y%m%d-%H%M%S.%%e "-FileName<DateTimeOriginal" .
-cd ~
+# Shutdown
 halt
