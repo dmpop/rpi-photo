a2953c80b1c9bed4b56a7612b826d787cea2cccb
Dmitri Popov <dmpop@linux.com>
2014-03-26 10:49:23 +0100

    Minor tweaks

--- rpi-photo-web/index.php
+++ rpi-photo-web/index.php
@@ -2,11 +2,16 @@
 
 	<head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<link href="style.css" rel="stylesheet" type="text/css" media="all" />
-	<link href='http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<link rel="shortcut icon" href="favicon.ico" />
+	<style type="text/css">
+	body { background-image:url('background.png'); font: 13px/175% 'Noto Sans', sans-serif; }
+	h1 { color: #999999; font: 39px 'Noto Sans', sans-serif; font-weight: 700; }
+        h2 { color: #999999; font: 19px 'Noto Sans', sans-serif; font-weight: 700; }
+        </style>
 
 	<title>RPi Photo Web</title>
+	
 	</head>
 
 	<body>
@@ -16,24 +21,24 @@
 	<?php
 	
 	echo "<form action='index.php' method='post'>
-					<select name='option'>
-					<option value='--version'>Version</option>
-					<option value='--help'>Help</option>
-					<option value='--abilities'>Show camera's abilities</option>
-					<option value='--list-config'>List configurable parameters</option>
-					<option value='--capture-image-and-download --filename \"%Y%m%d-%H%M%S-%03n.%C\"'>Capture-Download-Rename</option>
-					</select>
-					<input type='submit' value='Run'/>
-			</form>";
-			
-			$command='gphoto2 '.$_POST['option'];
-			echo shell_exec("$command");
-			echo "<br /><h2>Camera Summary</h2>";
-			echo shell_exec("gphoto2 --summary");
-			echo shell_exec("gphoto2 --reset");
-			
-			?>
-			
+			<select name='option'>
+			<option value='--version'>Version</option>
+			<option value='--help'>Help</option>
+			<option value='--abilities'>Show camera's abilities</option>
+			<option value='--list-config'>List configurable parameters</option>
+			<option value='--capture-image-and-download --filename \"%Y%m%d-%H%M%S-%03n.%C\"'>Capture-Download-Rename</option>
+			</select>
+			<input type='submit' value='Run'/>
+		</form>";
+	
+	$command='gphoto2 '.$_POST['option'];
+	echo shell_exec("$command");
+	echo "<br /><h2>Camera Summary</h2>";
+	echo shell_exec("gphoto2 --summary");
+	echo shell_exec("gphoto2 --reset");
+	
+	?>
+
 	</body>
 </html>
 
