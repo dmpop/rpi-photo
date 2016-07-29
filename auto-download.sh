fa6d1db0eee3a41e7ebd063d27eeb3f227d9bb57
Dmitri Popov <dmpop@linux.com>
2016-07-29 12:59:42 +0200

    Update auto-download.sh

--- auto-download.sh
+++ auto-download.sh
@@ -11,8 +11,9 @@ done
 # Switch to /media/usb0 and create a directory with current date as its name
 cd /media/usb0/
 mkdir "`date --iso-8601`" && cd $_
-# Transfer files to a USB storage device mounted at /media/usb0
-gphoto2 --get-all-files
+# Transfer new files to a USB storage device mounted at /media/usb0
+#gphoto2 --get-all-files
+gphoto2 --new
 # Rename files using ExifTool based on EXIF date and time data
 exiftool -r -d %Y%m%d-%H%M%S.%%e "-FileName<DateTimeOriginal" .
 # Shutdown
